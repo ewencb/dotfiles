be61f33a45fb0920ee324dee3d28aa909bd42ad7
cewen <me@cewen.io>
2024-12-02 19:56:46 +0000

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
+
+vim.keymap.set("n", "<C-w>h", "5<C-w>>")
+vim.keymap.set("n", "<C-w>j", "5<C-w>-")
+vim.keymap.set("n", "<C-w>k", "5<C-w>+")
+vim.keymap.set("n", "<C-w>l", "5<C-w><")
