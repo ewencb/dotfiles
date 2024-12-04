bcbeb0ea63aabbb092771928305585bbaa28001e
cewen <me@cewen.io>
2024-12-04 03:28:28 +0000

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocommands.lua Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/leap.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/mini.lua Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/nvim-cmp.lua Add .config/nvim/lua/plugins/nvim-lint.lua Add .config/nvim/lua/plugins/telescope.lua Add .config/nvim/lua/plugins/todo-comments.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/vim-sleuth.lua Add .config/nvim/lua/plugins/which-key.lua Update .config/nvim/lua/snippets/markdown.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,46 +1,6 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "<C-w>h", "5<C-w>>")
-vim.keymap.set("n", "<C-w>j", "5<C-w>-")
-vim.keymap.set("n", "<C-w>k", "5<C-w>+")
-vim.keymap.set("n", "<C-w>l", "5<C-w><")
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Define the custom command
-vim.api.nvim_create_user_command("CreateMarkdown", function()
-  -- Get the current line and cursor position
-  local line = vim.fn.getline(".")
-  local cursor_col = vim.fn.col(".")
-
-  -- Pattern to match all links ([[...]])
-  local links = {}
-  for link in line:gmatch("%[%[(.-)%]%]") do
-    table.insert(links, link)
-  end
-
-  -- Find the link under the cursor
-  local start_col = 0
-  local end_col = 0
-  local target_link = nil
-  for _, link in ipairs(links) do
-    start_col, end_col = line:find("%[%[" .. vim.pesc(link) .. "%]%]", start_col + 1)
-    if start_col and cursor_col >= start_col and cursor_col <= end_col then
-      target_link = link
-      break
-    end
-  end
-
-  if target_link then
-    -- Get the directory of the current file
-    local current_file_dir = vim.fn.expand("%:p:h") -- %:p:h gives the full path to the directory
-    local filepath = current_file_dir .. "/" .. target_link .. ".md"
-    -- Open or create the file in the same directory
-    vim.cmd("e " .. filepath)
-  else
-    print("No valid link found under the cursor.")
-  end
-end, {})
-
--- Map <leader>m to the CreateMarkdown command
-vim.keymap.set("n", "<leader>m", ":CreateMarkdown<CR>", { desc = "Create or Open Markdown Link under Cursor" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
-return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+return { "ellisonleao/gruvbox.nvim", priority = 1000,
+  init = function()
+  vim.o.background = "dark" -- or "light" for light mode
+  vim.cmd([[colorscheme gruvbox]])
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
-}
+  vim.cmd.hi 'Comment gui=none'
+  end
+  }
